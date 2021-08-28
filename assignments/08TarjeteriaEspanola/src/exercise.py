89f60b631465040dd21e8c2f4328cd91742ba381
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-27 22:03:04 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,18 @@
-
+import math
+def max():
+    papel= int(input("Dame la cantidad de pliegos de papel albanene:"))
+    plumones= int(input("Dame la cantidad de plumones:"))
+    if (papel>=plumones) or (papel*12<=35):
+        tarjetas= plumones*35
+        print("El número máximo de tarjetas que se pueden hacer es:", tarjetas)
+    elif (papel<plumones):
+        tarjetas= papel*12
+        print("El número máximo de tarjetas que se pueden hacer es:", tarjetas)
+    else:
+        print ("invalid")
 def main():
     #escribe tu código abajo de esta línea
+    max()
     pass
 
 if __name__=='__main__':
